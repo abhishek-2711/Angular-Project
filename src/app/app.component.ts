32dea5c518b6d76509a4639d32c6af16e6e3d942
Abhishek Rathod <rathod.abhishek2021@gmail.com>
2024-03-09 12:33:25 +0530

    Communication between components using event emmiters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  recipeSelected: boolean = true;
+  doNavigate(navigateTo: string){
+    if(navigateTo === 'shopping-list'){
+      this.recipeSelected = false;
+    }
+    else{
+      this.recipeSelected = true;
+    }
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-
+  @Output() selectedNavigation  = new EventEmitter<string>();
+  navigationHappened(navigationString: string){
+    this.selectedNavigation.emit(navigationString);
+  }
 }
