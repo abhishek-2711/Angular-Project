c0d0d3c68ac5f2b2640b202c258af5c15430d639
Abhishek Rathod <rathod.abhishek2021@gmail.com>
2024-04-06 06:43:36 +0530

    fix:(constructor added)

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -12,7 +12,13 @@ export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
   @Input() index: number;
 
-  ngOnInit() {
+  constructor(){
+    
   }
+  ngOnInit() {
+
+  }
+
+
 
 }
