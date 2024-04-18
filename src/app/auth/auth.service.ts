92a4c786f5258b7d416fe395bed7290e7c798143
Abhishek Rathod <rathod.abhishek2021@gmail.com>
2024-04-18 16:20:58 +0530

    added auto login feature

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -87,6 +87,31 @@ export class AuthService {
     const user = new User(email, localId, idToken, expirationDate);
 
     this.user.next(user);
+
+    localStorage.setItem('userData', JSON.stringify(user));
+  }
+
+  //   to retrieve data from local storage
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
   }
 
   private handleError(errorResponse: HttpErrorResponse) {
