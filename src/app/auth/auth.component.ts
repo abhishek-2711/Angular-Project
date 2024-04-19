f4a4b5bd3686bc2ba12b58ce6565fddc4804e65d
Abhishek Rathod <rathod.abhishek2021@gmail.com>
2024-04-19 09:44:00 +0530

    added dynamic component loading for showing alert modal

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,21 +1,36 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ComponentFactoryResolver,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthResponseData, AuthService } from './auth.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
   isLogInMode = true;
 
   isLoading = false;
 
   error: string = null;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  private closeSubscription: Subscription;
+
+  @ViewChild(PlaceholderDirective) alertHost: PlaceholderDirective;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) {}
   onSwithMode() {
     this.isLogInMode = !this.isLogInMode;
   }
@@ -45,9 +60,43 @@ export class AuthComponent {
       },
       (errorMessage) => {
         this.error = errorMessage;
+        this.showErrorAlert(this.error);
         this.isLoading = false;
       }
     );
     authForm.reset();
+  }
+
+  onHandleError() {
+    this.error = null;
+  }
+
+  private showErrorAlert(message: string) {
+    // dynamically create or instanciate the alert component
+
+    const alrertCmpFactoryResolver =
+      this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+
+    // now we can use this factory to create the instance of alert component and we need a place where we want to attach this component so for that we need viewContainerRef
+
+    // need to tell angular where we wanna add that
+
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+
+    const componentRef = hostViewContainerRef.createComponent(
+      alrertCmpFactoryResolver
+    );
+    componentRef.instance.message = message;
+    this.closeSubscription = componentRef.instance.close.subscribe(() => {
+      this.closeSubscription.unsubscribe();
+      hostViewContainerRef.clear();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.closeSubscription) {
+      this.closeSubscription.unsubscribe();
+    }
   }
 }
